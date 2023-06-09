a4c2cdd9a45949c9138154f11fb51bc0cb84a38e
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2023-06-08 23:40:15 -0400

    Add files via upload

---
+++ PlayingCat.java
@@ -0,0 +1,25 @@
+public class PlayingCat {
+
+    public static void main(String[] args) {
+
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+    }
+    public static boolean isCatPlaying(boolean summer, int temperature){
+
+//        summer = false;
+
+        if (!summer){
+            if(temperature >= 25 && temperature <= 35){
+                return true;
+            }
+        } else if (summer) {
+            if (temperature >= 25 && temperature <= 45){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
