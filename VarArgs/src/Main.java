b71d5bcdf30d8200a67f6ddb6ed03727f1e4c1c8
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2023-07-28 21:39:13 -0400

    Add files via upload

---
+++ VarArgs/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String... args) {
+
+        System.out.println("Hello world again!");
+
+        String[] splitStrings = "Hello World Again".split( " ");
+        printText(splitStrings);
+
+        System.out.println("_".repeat(20));
+        printText("Hello");
+
+        System.out.println("_".repeat(20));
+        printText("Hello", "World", "Again");
+
+        System.out.println("_".repeat(20));
+        printText();
+
+        String[] sArray = {"first", "second", "third", "fourth", "fifth"};
+        System.out.println(String.join(", ", sArray));
+    }
+
+    private static void printText(String... textList) {
+        for (String t : textList) {
+            System.out.println(t);
+        }
+    }
+}
