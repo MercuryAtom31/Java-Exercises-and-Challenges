fc3b8a3f6992b048bb2073fa761bff7312b61e4b
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2023-07-31 14:05:32 -0400

    Delete SortedArray directory

--- SortedArray/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("-".repeat(21) + "\nSorted Array Exercise\n" + "-".repeat(21));
-        System.out.println("\nSorting an array of integers in descending order\n" + "-".repeat(48));
-    }
-}
